220c1d09661475669617233944458131a35e7c85
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-10-13 17:27:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //move the image
 var img=document.getElementById("img");
 function moveRight(){
-    marginLeft=MarginLeft+1;
+    marginLeft=marginLeft+1;
 }
 img.onclick = function(){
   var interval= setInterval(moveRight,10);
