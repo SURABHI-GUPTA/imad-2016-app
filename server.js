6bb7afe9a6f6207fa6c97b77e563f36ebc932fa8
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-17 23:22:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+var Pool=require('pg').Pool;
+
+var config={
+    user: 'surabhi-gupta',
+    database: 'surabhi-gupta',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+  pool.query('SELECT * FROM test', function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  })  
+});
+
+
+
 var img = document .getElementById('img');
 var marginLeft=0;
 function moveRight(){
