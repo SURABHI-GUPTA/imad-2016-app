a2556cefd985096ed969c53b51f40d5b0e7dc761
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-17 19:17:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
-});
-
-app.get('/article-two',function(req,res){
-    res.send("Article two requested and will be served here.");
-});
-
-app.get('/article-three',function(req,res){
-    res.send("Article three requested and will be served here.");
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+var img = document .getElementById('img');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+}
+img.onclick= function(){
+    var interval= setInterval(moveRight,100);
+    };
+// last modified
+var msg = ' <b>last modified : </b> ' + document. lastModified + ' </p>' ;
+var el = document .getElementByld('side');
+el . innerHTML = msg ;
