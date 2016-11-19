9d3171249b4c6a72c5bcd4fab90b4e8c56cfdd64
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-19 18:10:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -254,16 +254,3 @@ var config={
 
 
 
-var img = document .getElementById('img');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick= function(){
-    var interval= setInterval(moveRight,100);
-    };
-// last modified
-var msg = ' <b>last modified : </b> ' + document. lastModified + ' </p>' ;
-var el = document .getElementByld('side');
-el . innerHTML = msg ;
