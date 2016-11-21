e2815da5910e02480edd1ce23240fbebae36c419
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-21 15:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 function hash (input, salt) {
     // How do we create a hash?
