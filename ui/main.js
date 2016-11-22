0e072aa8f29544fd82e65a7bceae77799127c0c0
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-22 17:28:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 function loadLoginForm () {
     var loginHtml = `
         <input type="text" id="username" placeholder="username" />
-        <input type="password" id="password" placeholder="password" /
+        <input type="password" id="password" placeholder="password" />
         </br></br>
         <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
