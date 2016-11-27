149dc6e6b050498d4ae086face39209631ecdfca
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-27 22:13:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,6 +112,10 @@ app.get('/ui/surabhi.jpg', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'surabhi.jpg'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 function hash (input, salt) {
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
