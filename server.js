dd490131b66e1bf8c7452271d061c931bda2c2b2
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-11-28 18:23:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,9 +76,6 @@ app.get('/ui/main.js', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/article.html', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article.html'));
-});
 
 app.get('/ui/bus.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bus.html'));
