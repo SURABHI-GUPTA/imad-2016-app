32f4409682c9a7d3465bb1c907fca0b47f3267da
SURABHI-GUPTA <surabhigupta021997@gmail.com>
2016-12-01 14:28:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,17 @@
-var read=`<p>
-       Sources said during the meeting chaired by the Congress president Sonia Gandhi, top party leaders were of the view that
-                            implementation of demonetisation move along with its alleged leakage to BJP should be questioned
-                            in both houses of Parliament.
-                             </p>
-                        `;
-//document.querySelector('button').addEventListener('click', function() {
-var btn = document.getElementById('btn');
-if (btn) {
-btn.addEventListener('click', function() {
-    document.getElementById('content').innerHTML = read;
-});
-}
+var currentArticleTitle = window.location.pathname.split('/')[2];
 
-function loadLoginForm () {
-   var loginHtml = ` 
-            <h2> Log In / Sign Up</h2>
-            <label for="username">Username:</label>
-             
-            <input type="text" id="username" name="username" placeholder="Your Username">
-             
-            <label for="password">Password:</label>
-             
-            <input type="password" id="password" name="password" placeholder="Your Password">
-             
-            <input type="submit" id="login_btn" value="Login" />
-            <input type="submit" id="register_btn" value="Register"/> `;
-            
-     document.getElementById('login_area').innerHTML = loginHtml;    
-        
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    
     // Submit username/password to login
-    var submit = document.getElementById('login_btn');
+    var submit = document.getElementById('submit');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -37,75 +19,75 @@ function loadLoginForm () {
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  submit.value = 'Success!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-              loadLogin();
-          }  
-          // Not done yet
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-        
-    };
-    
-    var register = document.getElementById('register_btn');
-    register.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
+                // Take some action
+                if (request.status === 200) {
+                    // clear the form & reload all the comments
+                    document.getElementById('comment_text').value = '';
+                    loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
           }
         };
         
         // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
+        var comment = document.getElementById('comment_text').value;
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
+        
     };
 }
 
-function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML = `
-        <a href="/logout">Logout</a>
-        <h3> Welcome <b>${username}</b></h3><br>
-        Create your own article.. <a href="/ui/article.html">Click here</a>
-        
-    `;
+function loadArticleForm() {
+	//added form tag and required attributes
+    var articleHtml = `
+        <h2>Your Article</h2>
+	<form id="article_form">
+        <input type="text" id="heading" placeholder="Heading" required/><br/>
+        <input type="text" id="title" placeholder="Title" required/><br/>
+        <input type="text" rows="4" cols="50" id="content" placeholder="Content"/><br/>
+        <input type="submit" id="save_btn" value="Create" />
+ 	</form>
+        `;
+        document.getElementById('article_area').innerHTML = articleHtml;
+        var store = document.getElementById('save_btn');
+	//handling onsubmit event of form
+ 	var article_form=document.getElementById('article_form');
+         article_form.onsubmit = function (e) {
+ 	e.preventDefault();//prevent default form submission
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  alert('Article created successfully');
+		  //location.reload(true);
+		  //reset the form
+		  article_form.reset();
+		  //now, load the articles dynamically
+		  loadArticles();
+              }
+              else if(request.status === 403){ //for alerting users to register/login to create article
+                alert('You must Register/Login to create new Article');
+	      } 
+              else {
+		  //use this alert message
+                  alert('Article could not be created or Article already exist!');
+              }
+		  //use this once here
+		  store.value = 'Create';
+          }
+        };
+	var heading = document.getElementById('heading').value;
+	var title = document.getElementById('title').value;
+	var content = document.getElementById('content').value;
+        request.open('POST', '/create-article', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({heading:heading, title:title , content:content}));  
+        store.value = 'Creating...';
+    };
 }
 
 function loadLogin () {
@@ -114,9 +96,7 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
+                loadCommentForm(this.responseText);
             }
         }
     };
@@ -125,47 +105,45 @@ function loadLogin () {
     request.send(null);
 }
 
-function getCounter(){
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
- request.open('GET', '/counter', true);
-    request.send(null);
+function escapeHTML (text)
+{
+    var $text = document.createTextNode(text);
+    var $div = document.createElement('div');
+    $div.appendChild($text);
+    return $div.innerHTML;
 }
 
-function loadArticles () {
+function loadComments () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
-            var articles = document.getElementById('articles');
+            var comments = document.getElementById('comments');
             if (request.status === 200) {
-                var content = '<ul>';
-                var articleData = JSON.parse(this.responseText);
-                for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
-                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
+                var content = '';
+                var commentsData = JSON.parse(this.responseText);
+                for (var i=0; i< commentsData.length; i++) {
+                    var time = new Date(commentsData[i].timestamp);
+                    content += `<div class="comment">
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <div class="commenter">
+                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                        </div>
+                    </div>`;
                 }
-                content += "</ul>";
-                articles.innerHTML = content;
+                comments.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!');
+                comments.innerHTML('Oops! Could not load comments!');
             }
         }
     };
     
-    request.open('GET', '/get-articles', true);
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
-             
-getCounter();
+
+
+// The first thing to do is to check if the user is logged in!
 loadLogin();
-loadArticles();
+loadComments();
+loadArticleForm();
